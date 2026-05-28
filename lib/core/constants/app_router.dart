b32827ee00314193a4e59b8e28dc0d749bbed77e
Trong <167620149+Bui-Minh-Trong@users.noreply.github.com>
2026-05-28 23:40:59 +0700

    feat(router): register my-reviews route and connect profile menu action

--- lib/core/constants/app_router.dart
+++ lib/core/constants/app_router.dart
@@ -11,6 +11,7 @@ import '../../screens/profile/about_app_screen.dart';
 import '../../screens/profile/report_issue_screen.dart';
 import '../../screens/profile/support_center_screen.dart';
 import '../../screens/profile/search_history_screen.dart';
+import '../../screens/profile/my_reviews_screen.dart';
 import '../../screens/auth/login_screen.dart';
 import '../../screens/auth/register_screen.dart';
 import '../../models/conversation.dart';
@@ -38,6 +39,7 @@ final appRouter = GoRouter(
     GoRoute(path: '/report-issue', builder: (_, __) => const ReportIssueScreen()),
     GoRoute(path: '/support-center', builder: (_, __) => const SupportCenterScreen()),
     GoRoute(path: '/search-history', builder: (_, __) => const SearchHistoryScreen()),
+    GoRoute(path: '/my-reviews', builder: (_, __) => const MyReviewsScreen()),
     GoRoute(path: '/chat',       builder: (_, __) => const ConversationsScreen()),
     GoRoute(
       path: '/chat/detail',
